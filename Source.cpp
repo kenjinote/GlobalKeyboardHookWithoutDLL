6fa87c5826d86df74f07d33e3f53207b0821c7d3
kenji <kenji256@gmail.com>
2016-06-25 12:38:06 +0900

    add files.

---
+++ Source.cpp
@@ -0,0 +1,86 @@
+﻿#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+HHOOK hHook;
+
+LRESULT CALLBACK KeyHookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode == HC_ACTION)
+	{
+		KBDLLHOOKSTRUCT*pKB = (KBDLLHOOKSTRUCT*)lParam;
+		if (pKB->vkCode == VK_ESCAPE)
+		{
+			if (wParam == WM_KEYDOWN)
+			{
+				keybd_event('A', 0, 0, 0);
+			}
+			else if (wParam == WM_KEYUP)
+			{
+				keybd_event('A', 0, KEYEVENTF_KEYUP, 0);
+			}
+			return 1;
+		}
+	}
+	return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hEdit;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyHookProc, NULL, 0);
+		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit, 10, 10, 256, 32, TRUE);
+		break;
+	case WM_DESTROY:
+		UnhookWindowsHookEx(hHook);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("DLLを使わないグローバルキーボードフック"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
